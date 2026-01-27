f3756cb9fa734fd1f685ab52c5c03672d0b11f62
Norude Sasayama <norude-sasayama@example.com>
2026-01-27 09:54:42 +0900

    篠山チルドレンズミュージアムの座標を正確に修正 (484C+F5)
    35.070580, 135.270580

--- app.js
+++ app.js
@@ -547,7 +547,7 @@ document.addEventListener('DOMContentLoaded', () => {
         },
         {
             name: { ja: '篠山チルドレンズミュージアム', en: 'Children\'s Museum' },
-            coords: [135.27058, 35.07058],
+            coords: [135.270580, 35.070580],
             address: { ja: '兵庫県丹波篠山市小立727', en: '727 Kodachi, Tamba Sasayama, Hyogo' },
             hours: { ja: '24時間', en: '24 Hours' },
             price: {
