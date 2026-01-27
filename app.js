588c196b785b0da83ddc6f0372817a2de93bcb67
Norude Sasayama <norude-sasayama@example.com>
2026-01-27 09:46:03 +0900

    施設名の修正: 篠山チルドレンズミュージアム

--- app.js
+++ app.js
@@ -546,7 +546,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         },
         {
-            name: { ja: '篠山チルドレンミュージアム', en: 'Children\'s Museum' },
+            name: { ja: '篠山チルドレンズミュージアム', en: 'Children\'s Museum' },
             coords: [135.2707, 35.0707],
             address: { ja: '兵庫県丹波篠山市小立727', en: '727 Kodachi, Tamba Sasayama, Hyogo' },
             hours: { ja: '24時間', en: '24 Hours' },
